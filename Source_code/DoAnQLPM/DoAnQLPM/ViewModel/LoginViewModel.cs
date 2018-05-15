b1a357b1b8e7d244aa8ebf8c11df97f857e4e539
phuongph1997 <phuongph1997@gmail.com>
2018-05-15 08:14:41 +0700

    up code c#

---
+++ Source_code/DoAnQLPM/DoAnQLPM/ViewModel/LoginViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Controls;
+using DoAnQLPM.Model;
+using System.Windows;
+namespace DoAnQLPM.ViewModel
+{
+    class LoginViewModel : BaseViewModel
+ {
+        public bool IsLoaded = false;
+        private string _TaiKhoan;
+        public string TaiKhoan { get { return _TaiKhoan; } set { _TaiKhoan = value; OnPropertyChanged(); } } 
+        private string _MatKhau;
+        public string MatKhau { get { return _MatKhau; } set { _MatKhau = value; OnPropertyChanged(); } } 
+
+        public ICommand LoginCommand { get; set; }
+        public ICommand PasswordChangedCommand { get; set; }
+        public LoginViewModel()
+        {
+
+            LoginCommand = new RelayCommand<object>((p) => { return true; }, showPatientAction);
+            PasswordChangedCommand = new RelayCommand<object>((p) => { return true; }, PasswordChangedAction);
+
+            //MessageBox.Show(DataProvider.Ins.DB.TaiKhoans.First().username);
+
+        }
+
+        public void showPatientAction(object obj)
+        {
+            var b = obj as PasswordBox;
+            _MatKhau = b.Password.ToString();
+            
+            var accCount = DataProvider.Ins.DB.TaiKhoans.Where(x => x.username == TaiKhoan  && x.password == _MatKhau).Count();
+            if (accCount > 0)
+            {
+                
+
+                MainWindow MainWindow = new MainWindow(); 
+     
+                MainWindow.ShowDialog();
+                
+
+                //p.Close();
+            }
+            else
+            {
+              //  IsLogin = false;
+                MessageBox.Show("Sai tài khoản hoặc mật khẩu!");
+            }
+
+         //   Application.Current.MainWindow = PatientWindow;
+         //   PatientWindow.Show();
+        }
+        public void PasswordChangedAction(object obj)
+        {
+            var b = obj as PasswordBox;
+            _MatKhau = b.Password.ToString();
+           // MessageBox.Show(b.Password.ToString());
+            //   Application.Current.MainWindow = PatientWindow;
+            //   PatientWindow.Show();
+        }
+     
+      
+    }
+}
---
+++ Source_code/DoAnQLPM/DoAnQLPM/ViewModel/MainViewModel.cs
@@ -0,0 +1,67 @@
+﻿using DoAnQLPM.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace DoAnQLPM.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public bool IsLoaded = false;
+        public ICommand PatientCommand { get; set; }
+        public ICommand StoreCommand { get; set; }
+        public ICommand ReportCommand { get; set; }
+        public ICommand ConfigCommand { get; set; }
+        public ICommand LoginCommand { get; set; }
+
+        public MainViewModel()
+        {
+
+            PatientCommand = new RelayCommand<object>((p) => { return true; }, showPatientAction);
+            StoreCommand = new RelayCommand<object>((p) => { return true; }, showStoreAction);
+            ReportCommand = new RelayCommand<object>((p) => { return true; }, showReportAction);
+            ConfigCommand = new RelayCommand<object>((p) => { return true; }, showConfigAction);
+            LoginCommand = new RelayCommand<object>((p) => { return true; }, showLoginAction);
+
+            //MessageBox.Show(DataProvider.Ins.DB.TaiKhoans.First().username);
+
+        }
+
+        public void showPatientAction(object obj)
+        {
+            PatientWindows PatientWindow = new PatientWindows(); PatientWindow.ShowDialog();
+         //   Application.Current.MainWindow = PatientWindow;
+         //   PatientWindow.Show();
+        }
+        public void showStoreAction(object obj)
+        {
+            StoreWindow StoreWindow = new StoreWindow(); StoreWindow.ShowDialog();
+         //   Application.Current.MainWindow = StoreWindow;
+         //   StoreWindow.Show();
+        }
+        public void showReportAction(object obj)
+        {
+            ReportWindow ReportWindow = new ReportWindow(); ReportWindow.ShowDialog();
+          //  Application.Current.MainWindow = ReportWindow;
+          //  ReportWindow.Show();
+        }
+        public void showConfigAction(object obj)
+        {
+            ConfigWindow ConfigWindow = new ConfigWindow(); ConfigWindow.ShowDialog();
+          //  Application.Current.MainWindow = ConfigWindow;
+          //  ConfigWindow.Show();
+        }
+        public void showLoginAction(object obj)
+        {
+            MainWindow MainWindow = new MainWindow();
+            MainWindow.ShowDialog();
+            //  Application.Current.MainWindow = ConfigWindow;
+            //  ConfigWindow.Show();
+        }
+      
+    }
+}
