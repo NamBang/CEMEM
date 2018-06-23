124524619ad72806e1c5a0de5bdfd0e10253008e
Nguyen Van Trua <15520934@gm.uit.edu.vn>
2018-06-23 21:11:03 +0700

    add Control

--- Source_code/DoAnQLPM/DoAnQLPM/ViewModel/MainViewModel.cs
+++ Source_code/DoAnQLPM/DoAnQLPM/ViewModel/MainViewModel.cs
@@ -2,10 +2,13 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace DoAnQLPM.ViewModel
@@ -13,9 +16,16 @@ namespace DoAnQLPM.ViewModel
     public class MainViewModel : BaseViewModel
     {
         public bool IsLoaded = false;
+        //Login
         public ICommand LoadedViewConmand { get; set; }
-        public ICommand AddCommand { get; set; }
-        public ICommand EditCommand { get; set; }
+        //Bệnh Nhân
+        public ICommand AddCommandBenhNhan { get; set; }
+        public ICommand EditCommandBenhNhan { get; set; }
+        public ICommand DeleteCommandBenhNhan { get; set; }
+        public ICommand IsOnCommandBenhNhan { get; set; }
+        public ICommand closeCommandBenhNhan { get; set; }
+        public ICommand TextChangedCommandBenhNhan { get; set; }
+        public ICommand ResetCommandBenhNhan { get; set; }
         public ICommand LapPhieuKhamBenhCommand { get; set; }
 
         public ObservableCollection<string> comboGioiTinh { get; set; }
@@ -40,6 +50,10 @@ namespace DoAnQLPM.ViewModel
                 }
             }
         }
+        private string _TimHoVaTen;
+        public string TimHoVaTen { get { return _TimHoVaTen; } set { _TimHoVaTen = value; OnPropertyChanged(); } }
+        private string _MaBN;
+        public string MaBN { get { return _MaBN; } set { _MaBN = value; OnPropertyChanged(); } }
         private string _HoVaTen;
         public string HoVaTen { get { return _HoVaTen; } set { _HoVaTen = value; OnPropertyChanged(); } }
         private string _GioiTinh;
@@ -48,14 +62,33 @@ namespace DoAnQLPM.ViewModel
         public int NamSinh { get { return _NamSinh; } set { _NamSinh = value; OnPropertyChanged(); } }
         private string _DiaChi;
         public string DiaChi { get { return _DiaChi; } set { _DiaChi = value; OnPropertyChanged(); } }
+        private bool IsOn = false;
+        //Báo Cáo
+        private bool isOnClick = false;
+        private int _Thang;
+        public int Thang { get { return _Thang; } set { _Thang = value; OnPropertyChanged(); } }
 
+        private DataView _allData;
+        public DataView allData
+        {
+            get { return _allData; }
+            set
+            {
+                if (value != _allData)
+                {
+                    _allData = value;
+                    OnPropertyChanged("allData");
+                }
+            }
+        }
 
-
-
+        public ICommand SelectionChangedCommandBaoCao { get; set; }
+        public ICommand ClickFindCommandBaoCao { get; set; }
+        public ICommand IsOnClick { get; set; }
         public MainViewModel()
         {
 
-
+            //Login
             LoadedViewConmand = new RelayCommand<Window>((p) => { return true; }, (p) =>
              {
                  IsLoaded = true;
@@ -78,12 +111,14 @@ namespace DoAnQLPM.ViewModel
                      p.Close();
                  }
              });
+
+            //Benh Nhan
             comboGioiTinh = new ObservableCollection<String>();
             comboGioiTinh.Add("Nam");
             comboGioiTinh.Add("Nữ");
 
             ListBenhNhan = new ObservableCollection<BenhNhan>(DataProvider.Ins.DB.BenhNhans);
-            AddCommand = new RelayCommand<object>((p) =>
+            AddCommandBenhNhan = new RelayCommand<object>((p) =>
             {
                 return true;
 
@@ -94,11 +129,11 @@ namespace DoAnQLPM.ViewModel
                 DataProvider.Ins.DB.BenhNhans.Add(Patient);
                 DataProvider.Ins.DB.SaveChanges();
 
-                ListBenhNhan.Add(Patient);
+                ListBenhNhan = new ObservableCollection<BenhNhan>(DataProvider.Ins.DB.BenhNhans);
             });
 
 
-            EditCommand = new RelayCommand<object>((p) =>
+            EditCommandBenhNhan = new RelayCommand<object>((p) =>
             {
                 if (SelectedItemBenhNhan == null)
                     return false;
@@ -114,6 +149,112 @@ namespace DoAnQLPM.ViewModel
                 Patient.NamSinh = NamSinh;
                 Patient.DiaChi = DiaChi;
                 DataProvider.Ins.DB.SaveChanges();
+
+            });
+
+
+            closeCommandBenhNhan = new RelayCommand<TextBox>((p) =>
+            {
+                return true;
+            }, (p) =>
+            {
+                p.Text = "";
+            });
+
+            IsOnCommandBenhNhan = new RelayCommand<Button>((p) =>
+            {
+                return IsOn;
+            }, (p) =>
+            {
+
+            });
+
+            TextChangedCommandBenhNhan = new RelayCommand<TextBox>((p) =>
+            {
+                return true;
+            }, (p) =>
+            {
+                _TimHoVaTen = p.Text;
+                if (_TimHoVaTen == "")
+                {
+                    ListBenhNhan = new ObservableCollection<BenhNhan>(DataProvider.Ins.DB.BenhNhans);
+                    IsOn = false;
+
+                }
+                else
+                {
+                    IsOn = true;
+                    string ConnStr = "Data Source=(local);Initial Catalog=QLPM4;Integrated Security=true;";
+                    using (SqlConnection conn = new SqlConnection())
+                    {
+                        conn.ConnectionString = ConnStr;
+                        conn.Open();
+                        SqlCommand command = new SqlCommand("[Search by Keyword]", conn);
+                        command.Parameters.Add("@keyword", SqlDbType.VarChar).Value = _TimHoVaTen;
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        ListBenhNhan.Clear();
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                while (reader.Read())
+                                {
+                                    string hovaten, gioitinh, diachi;
+                                    int namsinh;
+                                    hovaten = reader[1].ToString();
+                                    gioitinh = reader[2].ToString();
+                                    int.TryParse(reader[3].ToString(), out namsinh);
+                                    diachi = reader[4].ToString();
+                                    BenhNhan a = new BenhNhan() { HoVaTen = hovaten, GioiTinh = gioitinh, NamSinh = namsinh, DiaChi = diachi };
+                                    ListBenhNhan.Add(a);
+                                }
+                            }
+                            else
+                            {
+                                ListBenhNhan = new ObservableCollection<BenhNhan>(DataProvider.Ins.DB.BenhNhans.Where((x) => x.HoVaTen.StartsWith(_TimHoVaTen)));
+                            }
+
+                        }
+
+                    }
+                }
+            });
+
+            ResetCommandBenhNhan = new RelayCommand<object>((p) =>
+            {
+                return true;
+            }, (p) =>
+            {
+                HoVaTen = null;
+                GioiTinh = null;
+                NamSinh = 0;
+                DiaChi = null;
+                SelectedItemBenhNhan = null;
+            });
+
+            DeleteCommandBenhNhan = new RelayCommand<object>((p) =>
+            {
+                if (SelectedItemBenhNhan == null)
+                    return false;
+                var IdList = DataProvider.Ins.DB.BenhNhans.Where(x => x.MaBN == SelectedItemBenhNhan.MaBN);
+                if (IdList != null || IdList.Count() != 0)
+                    return true;
+                return false;
+            }, p =>
+            {
+                string ConnStr = "Data Source=(local);Initial Catalog=QLPM4;Integrated Security=true;";
+                using (SqlConnection conn = new SqlConnection())
+                {
+                    conn.ConnectionString = ConnStr;
+                    conn.Open();
+                    SqlCommand command = new SqlCommand("[Delete BenhNhan]", conn);
+                    command.Parameters.Add("@key", SqlDbType.Int).Value = SelectedItemBenhNhan.MaBN;
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.ExecuteNonQuery();
+                    ListBenhNhan = new ObservableCollection<BenhNhan>(DataProvider.Ins.DB.BenhNhans);
+                }
             });
 
             LapPhieuKhamBenhCommand = new RelayCommand<object>((p) =>
@@ -128,6 +269,45 @@ namespace DoAnQLPM.ViewModel
                 PhieuKhamBenhwindown.Show();
             });
 
+            //Báo Cáo
+            ClickFindCommandBaoCao = new RelayCommand<ListView>((p) =>
+            {
+                return true;
+            }, (p) =>
+            {
+                string ConnStr = "Data Source=(local);Initial Catalog=QLPM4;Integrated Security=true;";
+
+                using (SqlConnection conn = new SqlConnection())
+                {
+                    conn.ConnectionString = ConnStr;
+                    conn.Open();
+                    SqlCommand command = new SqlCommand("[Sales by Month]", conn);
+                    command.Parameters.Add("@month", SqlDbType.Int).Value = _Thang + 1;
+                    command.CommandType = CommandType.StoredProcedure;
+                    SqlDataAdapter ad = new SqlDataAdapter(command);
+                    DataTable dt = new DataTable();
+                    ad.Fill(dt);
+                    allData = dt.DefaultView;
+                }
+            }
+           );
+            IsOnClick = new RelayCommand<object>((p) =>
+            {
+                return isOnClick;
+            }, (p) =>
+            {
+
+            }
+           );
+            SelectionChangedCommandBaoCao = new RelayCommand<ComboBox>((p) =>
+            {
+                return true;
+            }, (p) =>
+            {
+                isOnClick = true;
+                _Thang = p.SelectedIndex;
+            }
+            );
 
         }
     }
