452b2326df2b1fc61de25cc7648e286cf20fa18c
QuocThi <30611784+QuocThi@users.noreply.github.com>
2018-06-23 10:47:00 +0700

    Update Kho Thuốc (StoreWindow) RUN

--- Source_code/DoAnQLPM/DoAnQLPM/Model/Model1.Designer.cs
+++ Source_code/DoAnQLPM/DoAnQLPM/Model/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\Online_Project\DoAn_CNPM\SE104_I23_Nhom_4_CEMEN_QLPMT\Source_code\DoAnQLPM\DoAnQLPM\Model\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\TQT\Desktop\Resposity NMCNPM\SE104_I23_Nhom_4_CEMEN_QLPMT\Source_code\DoAnQLPM\DoAnQLPM\Model\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
